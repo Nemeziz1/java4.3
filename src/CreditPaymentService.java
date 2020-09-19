b57961ed01a8995881b9a9cb084d7f6ab8400283
Andrey Koryakin <football2331lm@gmail.com>
2020-09-19 16:53:50 +0300

    Fixed

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
     public double calculate (double credit, double percent, double time) {
-        double payment = credit * ((percent) * (Math.pow((percent + 1), time)))/ (Math.pow((percent + 1), time) - 1);
+        double payment = credit * ((percent / 12 / 100) * (Math.pow((percent / 12 / 100 + 1), time))) / (Math.pow((percent / 12 / 100 + 1), time) - 1);
         return payment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double payment = service.calculate(1_000_000, 0.008325, 12);
+        double payment = service.calculate(1_000_000, 9.99, 12);
         int pay = (int)Math.round(payment);
         System.out.println(pay);
 
-        double payment1 = service.calculate(1_000_000, 0.008325, 24);
+        double payment1 = service.calculate(1_000_000, 9.99, 24);
         int pay1 = (int)Math.round(payment1);
         System.out.println(pay1);
 
-        double payment2 = service.calculate(1_000_000, 0.008325, 36);
+        double payment2 = service.calculate(1_000_000, 9.99, 36);
         int pay2 = (int)Math.round(payment2);
         System.out.println(pay2);
     }
