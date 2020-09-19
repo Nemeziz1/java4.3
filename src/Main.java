83c304ed1564054a89c9fa9e0558f2e13a43b59b
Andrey Koryakin <football2331lm@gmail.com>
2020-09-19 16:19:59 +0300

    Payment counted

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,15 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         double payment = service.calculate(1_000_000, 0.008325, 12);
-        System.out.println(payment);
+        int pay = (int)Math.round(payment);
+        System.out.println(pay);
 
         double payment1 = service.calculate(1_000_000, 0.008325, 24);
-        System.out.println(payment1);
+        int pay1 = (int)Math.round(payment1);
+        System.out.println(pay1);
 
         double payment2 = service.calculate(1_000_000, 0.008325, 36);
-        System.out.println(payment2);
+        int pay2 = (int)Math.round(payment2);
+        System.out.println(pay2);
     }
 }
